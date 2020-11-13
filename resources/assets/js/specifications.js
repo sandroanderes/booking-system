ab479a6a32f0668442bad2f9654e4e2c33d33778
baerni-im <beni.aebersold@gmail.com>
2020-11-13 22:46:38 +0100

    Switcher Fixe Reservationsdauer
    JS mit dem Switcher Fixe Reservationsdauer erstellen

--- resources/assets/js/specifications.js
+++ resources/assets/js/specifications.js
@@ -13,27 +13,27 @@ var timeunit_day = document.querySelector(
 
 //Inputfelder mit Minutenangaben
 var min_reservation_minute = document.querySelector(
-    '[id^="select2-field-specificationduration-min-min-"]'
-).parentElement.parentElement.parentElement.parentElement.parentElement;
+    '[id^="field-specificationduration-min-min-"]'
+).parentElement.parentElement;
 var max_reservation_minute = document.querySelector(
-    '[id^="select2-field-specificationduration-max-min-"]'
-).parentElement.parentElement.parentElement.parentElement.parentElement;
+    '[id^="field-specificationduration-max-min-"]'
+).parentElement.parentElement;
 
 //Inputfelder mit Stundenangaben
 var min_reservation_hour = document.querySelector(
-    '[id^="select2-field-specificationduration-min-h-"]'
-).parentElement.parentElement.parentElement.parentElement.parentElement;
+    '[id^="field-specificationduration-min-h-"]'
+).parentElement.parentElement;
 var max_reservation_hour = document.querySelector(
-    '[id^="select2-field-specificationduration-max-h-"]'
-).parentElement.parentElement.parentElement.parentElement.parentElement;
+    '[id^="field-specificationduration-max-h-"]'
+).parentElement.parentElement;
 
 //Inputfelder mit Tage-Angaben
 var min_reservation_day = document.querySelector(
-    '[id^="select2-field-specificationduration-min-d-"]'
-).parentElement.parentElement.parentElement.parentElement.parentElement;
+    '[id^="field-specificationduration-min-d-"]'
+).parentElement.parentElement;
 var max_reservation_day = document.querySelector(
-    '[id^="select2-field-specificationduration-max-d-"]'
-).parentElement.parentElement.parentElement.parentElement.parentElement;
+    '[id^="field-specificationduration-max-d-"]'
+).parentElement.parentElement;
 
 //EventListener Zeiteinheit
 timeunit_minute.addEventListener("click", reservation_timeunit);
@@ -47,16 +47,63 @@ min_reservation_hour.style.display = "none";
 max_reservation_hour.style.display = "none";
 min_reservation_day.style.display = "none";
 max_reservation_day.style.display = "none";
+
 // Function Show Hide TimeUnits
 function reservation_timeunit() {
+    min_reservation_minute.style.display = "none";
+    max_reservation_minute.style.display = "none";
+    min_reservation_hour.style.display = "none";
+    max_reservation_hour.style.display = "none";
+    min_reservation_day.style.display = "none";
+    max_reservation_day.style.display = "none";
+
     if (timeunit_minute.checked === true) {
-        min_reservation_minute.style.display = "flex";
-        max_reservation_minute.style.display = "flex";
-    } else if (timeunit_hour.checked === true) {
-        min_reservation_hour.style.display = "flex";
-        max_reservation_hour.style.display = "flex";
-    } else if (timeunit_hour.checked === true) {
-        min_reservation_day.style.display = "flex";
-        max_reservation_day.style.display = "flex";
+        min_reservation_minute.style.display = "inline";
+        max_reservation_minute.style.display = "inline";
+    }
+    if (timeunit_hour.checked === true) {
+        min_reservation_hour.style.display = "inline";
+        max_reservation_hour.style.display = "inline";
+    }
+    if (timeunit_day.checked === true) {
+        min_reservation_day.style.display = "inline";
+        max_reservation_day.style.display = "inline";
+    }
+}
+
+//Switcher Fixes Reservationsintervall
+var switcher_fixed_res = document.querySelector(
+    '[id^="field-specificationduration-fixed-"]'
+);
+
+//Selector Fixed duration 
+var select_fixed_dur = document.querySelector( 
+    '[id^="field-specificationtimeunit-"]'
+).parentElement.parentElement.parentElement.parentElement;
+
+
+//Button Min, Hour, Day
+var timeunit_button = document.querySelector( 
+    '[id^="field-specificationtimeunit-"]'
+).parentElement.parentElement.parentElement.parentElement;
+
+//EventListener Reservationsadauer
+switcher_fixed_res.addEventListener("click", reservation_duration_fixed);
+
+// Function Show Hide reservation_duration_fixed
+function reservation_duration_fixed() {
+    if (switcher_fixed_res.checked === true) {
+        min_reservation_minute.style.display = "none";
+        max_reservation_minute.style.display = "none";
+        min_reservation_hour.style.display = "none";
+        max_reservation_hour.style.display = "none";
+        min_reservation_day.style.display = "none";
+        max_reservation_day.style.display = "none";
+        
+        timeunit_button.style.display = "none";
+
+        //ne den Select einblenden
+        //ah ja, wie heisst der Min, Std aha - dann ist mein Comment falsch
+
     }
 }
